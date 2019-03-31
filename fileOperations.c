d363dc6c54af14975b54eca7594e76b2c3d6e88e
Agata <agata.rodzen@gmail.com>
2019-03-31 17:36:40 +0200

    Rozmiar siatki v3 (?) nieumiempamięci

--- fileOperations.c
+++ fileOperations.c
@@ -4,38 +4,40 @@
 
 static int rows, columns;
 
-int readFileBetter(char* inputfilename)
+int getSize(char* inputfilename)
 {
     FILE* f = fopen(inputfilename, "r");
 
     char* temp_rows = malloc ( sizeof (temp_rows) );
     char* temp_columns = malloc ( sizeof (temp_columns) );
-    char* inv_char = malloc ( sizeof (inv_char) );
 
     int *rows_p = &rows;
     int *columns_p = &columns;
     int i = 0;
-    int c = getc(f);
     int errors = 0;
+    char* inv_char = calloc (1000,sizeof (inv_char));
+    printf("%s", inv_char);
 
-    while ( c != '\n' && c != EOF)
+
+    int c = getc(f);
+    while ( c != '\n' && c != EOF)                                                      //check the validity of the characters in the first line
     {
-        if( !(isdigit(c)) && !(isspace(c)) )
+        if( !(isdigit(c)) && (!(isspace(c))) )
         {
             inv_char[errors] = c;
             errors++;
+
         }
         c = getc(f);
     }
 
-    rewind(f);
 
-    if( errors != 0)
+    rewind(f);
+    if( errors != 0)                                                                               //attempt to read the size only if there aren't any invalid characters
     {
-        printf("Error occurred while reading the grid size. \nCharacters: \"%s\" are invalid and should not appear in the first line of the file.\n", inv_char);
+        printf("Error occurred while reading the grid size. \nCharacters: %s are invalid and should not appear in the first line of the file.\n", inv_char);
         return 1;
     }
-
     else
     {
         c = getc(f);
@@ -50,7 +52,6 @@ int readFileBetter(char* inputfilename)
             c = getc(f);
         }
          *rows_p = atoi(temp_rows);
-
 
         while(isspace(c))
             c = getc(f);
@@ -69,94 +70,26 @@ int readFileBetter(char* inputfilename)
 
            if ( c == '\n' || c == EOF)
                 return 0;
-
            else
            {
                printf("Size of grid is not specified correctly. Edit the file to contain 2 numbers.\n");
                return 1;
            }
-
-
-
-
     }
-
     return 0;
 }
 
 
 
-    /*while(isspace(c))                                               //skips through any eventual spaces before the grid size
-        c = getc(f);
 
-    while(c != EOF)
-    {
-
-        while (c>= '0' && c<= '9')                           //reads the numbers
-        {
-            temp_rows[i]=c;
-            c=getc(f);
-        }
-        *rows_p = atoi(temp_rows);
-
-        if ( !(c>= '0' && c<= '9') && !(isspace(c)) )     //checks for invalid characters
-        {
-            inv_char[errors] = c;
-            errors++;
-        }
-
-
-
-        c = getc(f);
-
-        while ( isspace(c) )                                   //skips through any eventual spaces before the size of grid
-            c = getc(f);
-
-        while (c>= '0' && c<= '9')                          //reads the numbers
-        {
-            temp_columns[i] = c;
-            c = getc(f);
-        }
-
-        if ( !(c>= '0' && c<= '9') &&  !( isspace(c) ) )         //checks for invalid characters
-        {
-
-            inv_char[errors] = c;
-            errors++;
-        }
-
-        *columns_p = atoi(temp_columns);
-    }
-
-
-        if (errors != 0)
-        {
-            printf ("Found %d invalid characters while opening the file: %s", errors, inv_char);
-            return errors;
-        }
-
-        else
-            return 0;
-
-} */
 
 
 
 int main(int argc, char** argv)
 {
-    //int rows, columns;
-    //FILE* f;
-    //f = fopen("test.txt", "r");
-    //int rows, columns;
-    //fscanf(f, "%d %d", &rows, &columns);
-
-    //readFile("test.txt");
-
-    readFileBetter("test.txt");
+    if(!getSize("test.txt"))
     printf("%d %d", rows, columns);
 
-
-    //int g = getchar();
     return 0;
 }
 
